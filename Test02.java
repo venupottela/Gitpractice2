cd87036fe9ed47e414d208b8adab3d5cbfdd5050
venu <gvenu350@gmail.com>
2022-08-18 11:33:51 +0530

    add one syst status

--- Test02.java
+++ Test02.java
@@ -1,6 +1,7 @@
 public class Test02 {
 
 	public static void main(String[] args) {
-		System.out.println("hii how are");
+		System.out.println("where are you");
+                System.out.println("hii");
 	}
 }
