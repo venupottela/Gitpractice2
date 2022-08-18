6c194b379a59a439f96015e4f4fe7d1be6363572
venu <gvenu350@gmail.com>
2022-08-18 11:55:05 +0530

    updated test02 syst

--- Test02.java
+++ Test02.java
@@ -2,6 +2,9 @@ public class Test02 {
 
 	public static void main(String[] args) {
 		System.out.println("where are you");
+                System.out.println("sunil hii");
+                System.out.println("abhilash how are you");
+                System.out.println("kamesh what about your self");
                 System.out.println("hii");
 	}
 }
