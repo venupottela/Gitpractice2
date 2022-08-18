92605b63618b384874ac89ff4080ac2d0d68fcad
venu <gvenu350@gmail.com>
2022-08-18 12:02:56 +0530

    update in syst statement

--- Test02.java
+++ Test02.java
@@ -2,9 +2,9 @@ public class Test02 {
 
 	public static void main(String[] args) {
 		System.out.println("where are you");
-                System.out.println("sunil hii");
+                System.out.println("sunil brave boy and intelgent boy");
                 System.out.println("abhilash how are you");
-                System.out.println("kamesh what about your self");
+                System.out.println("kamesh what about your self and you can change the behaviour");
                 System.out.println("hii");
 	}
 }
