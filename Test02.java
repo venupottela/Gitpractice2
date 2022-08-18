ddea67560f99fbc32e62d04dcb80da288e12a8cc
venu <gvenu350@gmail.com>
2022-08-18 22:15:22 +0530

    add one syst statement

--- Test02.java
+++ Test02.java
@@ -2,9 +2,7 @@ public class Test02 {
 
 	public static void main(String[] args) {
 		System.out.println("where are you");
-                System.out.println("sunil brave boy and intelgent boy");
-                System.out.println("abhilash how are you");
-                System.out.println("kamesh what about your self and you can change the behaviour");
+                system.out.println("venu is searching for job");
                 System.out.println("hii");
 	}
 }
